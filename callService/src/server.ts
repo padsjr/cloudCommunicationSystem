83bfa11ff1a03c6ef52e01f724d57e1d915c456b
padsjr <107127638+padsjr@users.noreply.github.com>
2026-03-08 20:40:26 -0300

    /feat configurado servidor Fastify para o servico de comunicacao

---
+++ callService/src/server.ts
@@ -0,0 +1,16 @@
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+    logger: true,
+})
+fastify.get('/', async () => { return { status: 'Serviço de comunicação rodando' }; });
+
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+start();
